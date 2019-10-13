686f5c990bc9a97b23ca8d9ded40123e1e234d4e
Alex Ellis <alexellis2@gmail.com>
2019-10-13 08:56:12 +0100

    Bump build script
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t alexellis2/minio-kv:0.1.1 .
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t alexellis2/minio-kv:0.2.0 .
 
